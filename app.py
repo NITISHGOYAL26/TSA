9ac0d3c6bdeb39e10483a137fdad43d08883d931
NITISHGOYAL26 <112878075+NITISHGOYAL26@users.noreply.github.com>
2023-07-17 14:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def index():
 
 @app.route('/analyse', methods=['POST'])
 def analyse():
-    my_user="NavdeeeeepKaur"
-    my_pass="Trustnoone@123"
+    my_user=" "
+    my_pass=" "
     sleep(3)
     if request.method=='POST':
         search_item=request.form['rawtext']
